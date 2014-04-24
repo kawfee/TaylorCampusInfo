734047c4c1333d718e25bafdb6ed0126a194c5ac
Alex Sjoberg <asjoberg22@yahoo.com>
2014-04-23 21:39:58 -0400

    First commit of parsing DC feed. Not functional

---
+++ Website/js/feed_parsing.js
@@ -0,0 +1,25 @@
+// Alex Sjoberg
+// feed_parsing.js
+// 4/23/14
+
+//http://cse.taylor.edu/~ansmith/DCMenu
+
+window.addEventListener("load" , init , false) 
+var admin = false;
+
+function init(){
+	console.log("looking for data from stream");
+
+	//send request to andrew's page
+	var xhr = new XMLHttpRequest();
+	xhr.open ("GET" , "http://cse.taylor.edu/~ansmith/DCMenu");
+
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4 && xhr.status === 200){
+			console.log("got response");
+		}
+	}
+
+	xhr.send();
+}
+
